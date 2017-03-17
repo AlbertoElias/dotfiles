dde9cff62d46c83ff54bb330c12eace3dbcf1fb1
Alberto Elias <hi@albertoelias.me>
2017-03-17 09:05:08 +0100

    More improvements

--- src/os/install/nvm.sh
+++ src/os/install/nvm.sh
@@ -20,9 +20,6 @@ export NVM_DIR=\"$NVM_DIRECTORY\"
 
 [ -f \"\$NVM_DIR/nvm.sh\" ] \\
     && . \"\$NVM_DIR/nvm.sh\"
-
-[ -f \"\$NVM_DIR/bash_completion\" ] \\
-    && . \"\$NVM_DIR/bash_completion\"
 "
 
     # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
